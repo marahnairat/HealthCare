055799c5f221b87a4742c3bd41b7b07380830901
marahnairat <marahnairat0@gmail.com>
2020-10-14 13:23:59 +0300

    add the reminder of symptom section and editing on the choose section menu

--- app/src/main/java/com/example/healthcare/choose_symptom.java
+++ app/src/main/java/com/example/healthcare/choose_symptom.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.RadioButton;
 
 public class choose_symptom extends AppCompatActivity {
@@ -13,21 +14,35 @@ public class choose_symptom extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choose_symptom);
-    }
-    public void onRadioButtonClicked(View view) {
-        // Is the button now checked?
-        boolean checked = ((RadioButton) view).isChecked();
 
-        // Check which radio button was clicked
-        switch(view.getId()) {
-            case R.id.radioButton1:
-                if (checked)
-                    startActivity(new Intent(this,section1.class));
-                break;
-            case R.id.radioButton2:
-                if (checked)
-                    // Ninjas rule
-                    break;
-        }
+    }
+
+
+    public void opensection1(View view) {
+        startActivity(new Intent(choose_symptom.this, section1.class));
+    }
+
+    public void opensection7(View view) {
+        startActivity(new Intent(choose_symptom.this, section7.class));
+    }
+
+    public void opensection6(View view) {
+        startActivity(new Intent(choose_symptom.this, section6.class));
+    }
+
+    public void opensection5(View view) {
+        startActivity(new Intent(choose_symptom.this, section5.class));
+    }
+
+    public void opensection4(View view) {
+        startActivity(new Intent(choose_symptom.this, section4.class));
+    }
+
+    public void opensection3(View view) {
+        startActivity(new Intent(choose_symptom.this, section3.class));
+    }
+
+    public void opensection2(View view) {
+        startActivity(new Intent(choose_symptom.this, section2.class));
     }
 }
