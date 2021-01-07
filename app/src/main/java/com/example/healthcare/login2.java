6f9b906c71a51b2a57b8b9924c844d56659b8d9d
marahnairat <marahnairat0@gmail.com>
2021-01-07 21:20:49 +0300

    support doctor and user login

---
+++ app/src/main/java/com/example/healthcare/login2.java
@@ -0,0 +1,112 @@
+package com.example.healthcare;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+public class login2 extends AppCompatActivity {
+
+    EditText email,password;
+    Button loginBtn,gotoRegister;
+    boolean valid = true;
+    FirebaseAuth fauth;
+    FirebaseFirestore fstore;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login2);
+
+          fauth=FirebaseAuth.getInstance();
+          fstore=FirebaseFirestore.getInstance();
+
+        email = findViewById(R.id.loginEmail);
+        password = findViewById(R.id.loginPassword);
+        loginBtn = findViewById(R.id.loginBtn);
+        gotoRegister = findViewById(R.id.gotoRegister);
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                checkField(email);
+                checkField(password);
+                if (valid) {
+                    fauth.signInWithEmailAndPassword(email.getText().toString(), password.getText().toString()).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                        @Override
+                        public void onSuccess(AuthResult authResult) {
+                            Toast.makeText(login2.this,"Loggedin successfully  ",Toast.LENGTH_SHORT).show();
+                            CheckUserAccessLevel(authResult.getUser().getUid());
+
+                        }
+                    }).addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+
+                        }
+                    });
+
+
+                }
+            }
+        });
+
+     gotoRegister.setOnClickListener(new View.OnClickListener() {
+         @Override
+         public void onClick(View view) {
+             startActivity(new Intent(getApplicationContext(),register.class));
+         }
+     });
+
+
+
+    }
+
+    private void CheckUserAccessLevel(String uid) {
+        DocumentReference df =fstore.collection("Users").document(uid);
+        df.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+            @Override
+            public void onSuccess(DocumentSnapshot documentSnapshot) {
+                Log.d("TAG", "onSuccess:" + documentSnapshot.getData());
+                if(documentSnapshot.getString("isDoctor")!=null){
+                    startActivity(new Intent(getApplicationContext(),Dr_profile.class));
+                    finish();
+                }
+                if(documentSnapshot.getString("isUser")!=null){
+                    startActivity(new Intent(getApplicationContext(),CheckActivity.class));
+                    finish();
+
+
+                }
+
+
+            }
+        });
+    }
+
+    public boolean checkField(EditText textField){
+        if(textField.getText().toString().isEmpty()){
+            textField.setError("Error");
+            valid = false;
+        }else {
+            valid = true;
+        }
+
+        return valid;
+    }
+
+}
