d17cf10c1cc76fe101918072a14167bc4ddf322f
marah <marahnairat0@gmail.com>
2021-01-24 00:04:02 +0200

    location hiba

--- app/src/main/java/com/example/healthcare/mycurrentlocation.java
+++ app/src/main/java/com/example/healthcare/mycurrentlocation.java
@@ -22,30 +22,10 @@ import com.google.android.gms.location.LocationRequest;
 
 import java.util.List;
 import java.util.Locale;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
-
-import android.Manifest;
-import android.annotation.SuppressLint;
-import android.content.pm.PackageManager;
-import android.location.Address;
-import android.location.Geocoder;
-import android.location.Location;
-import android.location.LocationListener;
-import android.location.LocationManager;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
-import android.widget.Toast;
-
-import com.google.android.gms.location.LocationRequest;
-
-import java.util.List;
-import java.util.Locale;
 
 public class mycurrentlocation extends AppCompatActivity implements LocationListener {
+
+
 
     Button button_location;
     TextView textView_location;
