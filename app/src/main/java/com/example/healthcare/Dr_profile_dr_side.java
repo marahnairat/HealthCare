de2370801fee9242b5d42f9be1282779e5fb9b68
marah <marahnairat0@gmail.com>
2021-01-29 21:21:46 +0200

    design doctor profile habosha

--- app/src/main/java/com/example/healthcare/Dr_profile_dr_side.java
+++ app/src/main/java/com/example/healthcare/Dr_profile_dr_side.java
@@ -1,5 +1,6 @@
 package com.example.healthcare;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -21,11 +22,17 @@ public class Dr_profile_dr_side extends AppCompatActivity {
         d_city=findViewById(R.id.d_address);
         d_phone= findViewById(R.id.phone_num);
         d_specialization=findViewById(R.id.d_spec);
-
+        Intent intent = getIntent();
+        String phone= intent.getStringExtra("doctor_phone");
+        d_phone.setText(phone);
+        String name= intent.getStringExtra("doctor_name");
+        d_name.setText(name);
         Button show_appointment = findViewById(R.id.show_appointment);
         show_appointment.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+
 
             }
         });
